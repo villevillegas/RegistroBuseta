1430c99df99bac3ffb6aae477b5ad51cf2916a59
villevillegas <villevillegas2@gmail.com>
2015-02-15 11:14:13 -0600

    probando

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         
+        <title></title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <link rel="stylesheet" href="bootstrap-3.3.2-dist/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="bootstrap-3.3.2-dist/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="bootstrap-3.3.2-dist/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <div class="row">
+            <div class="col-lg-4 alert-danger">COLUMNA 1</div>
+            <div class="col-md-4 alert-info">COLUMNA 2</div>
+            <div class="col-md-4 alert-success">COLUMNA 3</div>
+        </div>
+        <?php
+        echo '<p>funciona</p>';
+        ?>
+    </body>
+</html>
